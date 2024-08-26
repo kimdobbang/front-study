f99e0df71f18079e87f3736c362a5e18c809be1e
kimdoi <kimdoiary@gmail.com>
2024-08-26 16:12:36 +0900

    0826

--- onebite-react/section01/chapter10.js
+++ onebite-react/section01/chapter10.js
@@ -1,10 +1,10 @@
 for (let idx = 1; idx <= 10; idx++) {
   if (idx % 2 === 0) {
-    continue;
+    continue; // 조건식 참일 때 건너 뛰기
   }
   console.log(idx);
 
   if (idx >= 5) {
-    break;
+    break; // 강제종료
   }
 }
