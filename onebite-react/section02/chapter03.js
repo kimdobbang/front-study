6d12615c27b65e6b58813681dfbf518b97838a0c
kimdoi <kimdoiary@gmail.com>
2024-09-02 01:13:10 +0900

    0901

--- onebite-react/section02/chapter03.js
+++ onebite-react/section02/chapter03.js
@@ -1,18 +1,27 @@
 // 1. 배열의 구조 분해 할당
 let arr = [1, 2, 3];
 
-let [one, two, three, four = 4] = arr;
+let [one, two, three, four = 4, five] = arr;
+console.log(one, two, three, four, five); // 1 2 3 4 undefined
 
-// 2. 객체의 구조 분해 할당
+// 2. 객체의 구조 분해 할당 (객체의 프로퍼티를 키값 기준으로 변수에할당)
+// 배열 구조분해할당과 차이: 객체를 뜻하는 중괄호 사용
+// 함수에 여러 인자 전달시 사용
 let person = {
-  name: "이정환",
-  age: 27,
+  name: "김도이",
+  age: 29,
   hobby: "테니스",
 };
 
-let { age: myAge, hobby, name, extra = "hello" } = person;
+let { age: myAge, hobby, name, extra = "hello", extra2 } = person;
+console.log(name, myAge, hobby, extra, extra2);
 
 // 3. 객체 구조 분해 할당을 이용해서 함수의 매개변수를 받는 방법
+const func2 = ({ name, age, hobby }) => {
+  console.log(name, age, hobby, extra);
+};
+func2(person);
+
 const func = ({ name, age, hobby, extra }) => {
   console.log(name, age, hobby, extra);
 };
--- onebite-react/section02/chapter04.js
+++ onebite-react/section02/chapter04.js
@@ -4,27 +4,34 @@
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, ...arr1, 5, 6];
+// console.log(arr2);
 
 let obj1 = {
   a: 1,
   b: 2,
 };
+
 let obj2 = {
+  // a: obj1.a,
+  // b: obj1.b,
   ...obj1,
   c: 3,
   d: 4,
 };
+// console.log(obj2);
 
+// 함수 호출시 스프레드 연산자 활용
 function funcA(p1, p2, p3) {
-  //   console.log(p1, p2, p3);
+  // console.log(p1, p2, p3);
 }
 
 funcA(...arr1);
 
-// 2. Rest 매개변수
-// -> Rest는 나머지 , 나머지 매개변수
+// 2. Rest 매개변수 (배열 형태로 여러 매개변수를 한 번에 받음)
+// -> Rest는 나머지 , 나머지 매개변수, rest매개변수는 맨 뒤에만 올 수 있다. 뒤에 다른 변수 추가 안됨
 
 function funcB(one, two, ...ds) {
+  // 뒤에 , four는 불가
   console.log(ds);
 }
 
